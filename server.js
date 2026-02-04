4dda259b0c8bee3418edc60aceaa948c1776bd2c
Jefernee <jefernee50@gmail.com>
2026-02-04 10:48:37 -0600

    probar coldstart

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ import authRoutes from "./routes/auth.js";
 import salesRoutes from "./routes/sales.js";
 import pedidosRoutes from "./routes/pedidos.js";
 import reportsRoutes from "./routes/reports.js";
+// ⏱️ Marca de inicio real del proceso (cold start)
+const PROCESS_START_TIME = Date.now();
 
 dotenv.config();
 
 console.log("✅ FRONTEND_URL:", process.env.FRONTEND_URL);
 
 const app = express();
+let firstRequest = true;
 const PORT = process.env.PORT || 5000;
 
 // Permitir localhost SIEMPRE (desarrollo y producción)
@@ -85,10 +88,17 @@ app.listen(PORT, () => {
 });
 
 app.get("/api/health", (req, res) => {
+  if (firstRequest) {
+    const coldStartTime = Date.now() - PROCESS_START_TIME;
+    console.log("❄️ COLD START DETECTADO");
+    console.log(`⏱️ Tiempo hasta primer request: ${coldStartTime} ms`);
+    firstRequest = false;
+  }
+
   res.status(200).json({
     status: "healthy",
     timestamp: new Date().toISOString(),
-    uptime: process.uptime(), // Tiempo que lleva corriendo
+    uptime: process.uptime(),
   });
 });
 
